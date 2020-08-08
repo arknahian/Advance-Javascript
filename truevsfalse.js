f0c16dcfdec50fab4d5e7fbcc2e349885ed5a3f5
Al Nahian Ark <63243119+arknahian@users.noreply.github.com>
2020-08-08 14:48:56 +0600

    this is almost done

---
+++ truevsfalse.js
@@ -0,0 +1,16 @@
+// flasy
+// const name = 0;
+// const empty = "";
+// const bullyan = false;
+
+//truely
+const emptySpace = " ";
+
+
+
+if (emptySpace){
+    console.log("condition is true");
+}
+else {
+    console.log("condition is not true");
+}
